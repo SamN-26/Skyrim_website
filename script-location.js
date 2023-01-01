06809a87f1237ec12c7f250b4baf6e7d1c5989bc
Samriddh Negi <samridh.negi007@gmail.com>
2023-01-01 21:49:43 +0530

    Finished Locations Page

---
+++ script-location.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panels')
+
+panels.forEach(panel =>{
+    panel.addEventListener('click', ()=>{
+        removeActionClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActionClasses()
+{
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+        
+    })
+}
